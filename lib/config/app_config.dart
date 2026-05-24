6ba4d7e0ebc919e76a3af9c3288c91606b974413
Bhaskar Pait <bhaskarpait44@gmail.com>
2026-05-25 01:53:45 +0530

    add address

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,6 +1,6 @@
 class AppConfig {
   static const String appName = 'EduCore Student';
-  static const String serverIp = '10.137.4.32';
+  static const String serverIp = '10.241.71.32';
   static const String apiBaseUrl = 'http://$serverIp:5000/api';
   static const String uploadsUrl = 'http://$serverIp:5000/uploads';
 }
